5097e84ba26f786523b13c6b56e0a9aea63315c2
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-07-05 21:11:36 -0500

    (add) add project

---
+++ src/main/groovy/com/example/groovymisc/GroovyMiscApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.groovymisc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GroovyMiscApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GroovyMiscApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/groovymisc/GroovyMiscApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.groovymisc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroovyMiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
